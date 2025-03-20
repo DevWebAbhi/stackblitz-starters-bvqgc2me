b7490bcfced475caf64bfbe30fba82508e085e60
DevWebAbhi <tiwariabhishek889912@gmail.com>
2025-03-20 10:43:26 +0530

    Done

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3010;
+
+require("dotenv").config();
 
 app.use(express.static('static'));
 
@@ -10,6 +13,14 @@ app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(port, async() => {
+  try {
+    
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(" Connected to database")
+    console.log(`Example app listening at http://localhost:${port}`);
+  } catch (error) {
+    console.log(error)
+  }
+  
 });
